113e15c10c597e499ec4b01e440067c410b8281e
ArunkumarJ325 <jarunkumar325@gmail.com>
2024-05-14 12:52:39 +0530

    Leetcode 1108

---
+++ Leetcode/Strings/ipaddress.java
@@ -0,0 +1,18 @@
+package Strings;
+
+
+class ipaddress{
+    public String defangIPaddr(String address) {
+        StringBuilder s=new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                s.append("[.]");
+            }
+            else{
+                s.append(address.charAt(i));
+            }
+        }
+        return s.toString();
+    }
+}
+//Leetcode 1108
