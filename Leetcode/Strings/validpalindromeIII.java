a5d123b1290668186a4729f9858dbf46d0cde0d1
ArunkumarJ325 <jarunkumar325@gmail.com>
2024-05-16 15:53:20 +0530

    Leetcode 680

---
+++ Leetcode/Strings/validpalindromeIII.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class validpalindromeIII {
+   
+    public boolean validPalindrome(String s) {
+        int i=0 ,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else {
+                return palindrome(s,i+1,j)||palindrome(s,i,j-1);
+            }
+        }  
+        return true;
+    }
+    public boolean palindrome(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
