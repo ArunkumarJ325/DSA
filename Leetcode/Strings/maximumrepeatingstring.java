e3cb496938f93dfb200d3c1181dcf535403a63c9
ArunkumarJ325 <jarunkumar325@gmail.com>
2024-05-17 11:59:04 +0530

    Leetcode 1668

---
+++ Leetcode/Strings/maximumrepeatingstring.java
@@ -0,0 +1,15 @@
+package Strings;
+public class maximumrepeatingstring {
+    
+        public int maxRepeating(String sequence, String word) {
+            int count=0;
+            StringBuilder sb=new StringBuilder();
+            sb.append(word);
+            while(sequence.contains(sb)){
+                count++;
+                sb.append(word);
+            }
+            return count;
+        }
+    }
+
