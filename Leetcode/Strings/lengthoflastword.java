f6f1988cadaca0a829a5441dd0a8f9f0f76d9e6d
ArunkumarJ325 <jarunkumar325@gmail.com>
2024-05-17 14:07:28 +0530

    Leetcode 58

---
+++ Leetcode/Strings/lengthoflastword.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class lengthoflastword {
+   
+    public int lengthOfLastWord(String s) {
+        int length=0;
+        s=s.trim();
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                length++;
+            }
+            else if(s.charAt(i)==' ') break;
+
+        }
+        return length;
+    }
+}
+
